cd404f3ed36c8c139a88a9a228d0f0536ac7283d
“AdigunaHakim” <adiguna.hakim@gmail.com>
2021-03-22 01:04:45 +0700

    add controller kompas

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const homeRouter = require('./home');
 const plRouter = require('./pl');
 const detikRouter = require('./detik');
+const kompasRoouter = require('./kompas');
 
 module.exports = (app) => {
     app.use(plRouter);
     app.use(detikRouter);
+    app.use(kompasRoouter);
     homeRouter(app);
 }
