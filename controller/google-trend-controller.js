71eea95cf06cb6740ae520ac2ed79c4a3abc7da7
“AdigunaHakim” <adiguna.hakim@gmail.com>
2021-03-22 12:37:55 +0700

    add google trend contry JP,GB,SG & IN

--- controller/google-trend-controller.js
+++ controller/google-trend-controller.js
@@ -3,13 +3,17 @@ const createError = require('http-errors');
 
 const country = {
     ID: '?geo=ID',
-    US: '?geo=US'
+    US: '?geo=US',
+    JP: '?geo=JP',
+    GB: '?geo=GB',
+    SG: '?geo=SG',
+    IN: '?geo=IN'
 }
 const getTopTopics = (req, res, next) => {
     const path = country[req.params.country.toUpperCase()];
 
     if(!path){
-        createError(404);
+        next(createError(404));
     }
 
     scrapTopTopics(path)
