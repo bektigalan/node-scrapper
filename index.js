9c5faa55d2519b3a7cb6a5eb3e0a60547692d117
Adiguna Hakim <adiguna.hakim@btpn.com>
2021-05-08 01:22:28 +0700

    load port with env

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
     console.log("server is running");
 });
